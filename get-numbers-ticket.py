f5f8eca439b3a2338dab782a3dfaaacdada340a4
StanislavYurchenko <yurchenko.stanislav@ukr.net>
2026-01-29 00:44:02 +0200

    hw-3 get-numbers-ticket added sorting

--- get-numbers-ticket.py
+++ get-numbers-ticket.py
@@ -22,7 +22,7 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
     if quantity > (max - min + 1) or min < 1 or max > 1000:
         raise ValueError("Quantity exceeds the range of unique numbers available. min must be >= 1 and max must be <= 1000.")
 
-    return random.sample(range(min, max + 1), quantity)
+    return sorted(random.sample(range(min, max + 1), quantity))
 
 # Example usage:
 numbers = get_numbers_ticket(1, 50, 10)
